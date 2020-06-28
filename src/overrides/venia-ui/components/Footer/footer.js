a7d84fba0547cd0d7c1ce755ac4327dcbc66238d
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-06-29 10:13:45 +1200

    Fooman footer, needs token update

---
+++ src/overrides/venia-ui/components/Footer/footer.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import { shape, string } from 'prop-types';
+import { useFooter } from '@magento/peregrine/lib/talons/Footer/useFooter';
+
+import { mergeClasses } from '@magento/venia-ui/lib/classify';
+import defaultClasses from './footer.css';
+import GET_STORE_CONFIG_DATA from '@magento/venia-ui/lib/queries/getStoreConfigData.graphql';
+import Icon from '@magento/venia-ui/lib/components/Icon';
+import {Linkedin as LinkedinIcon, Twitter as TwitterIcon} from "react-feather";
+import { ShowAt, HideAt } from 'react-with-breakpoints';
+import { Link, resourceUrl } from '@magento/venia-drivers';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+
+const Footer = props => {
+    const classes = mergeClasses(defaultClasses, props.classes);
+
+    const talonProps = useFooter({
+        query: GET_STORE_CONFIG_DATA
+    });
+    const { copyrightText } = talonProps;
+
+    let copyright = null;
+    if (copyrightText) {
+        copyright = <span>{copyrightText}</span>;
+    }
+
+    return (
+        <footer className={classes.root}>
+            <section className={classes.tile}>
+                <p className={classes.tileBody}>
+                    Copyright Fooman
+                </p>
+                <a style={classes.link} href={'https://www.linkedin.com/in/kristofringleff'}>
+                    <FontAwesomeIcon icon={faLinkedin} color={'white'} size={'2x'}/>
+                </a>
+                <a style={classes.link} href={'https://twitter.com/foomanNZ'}>
+                <FontAwesomeIcon icon={faTwitterSquare} color={'white'} size={'2x'}/>
+                </a>
+
+                <p>
+                    Proud member of
+                </p>
+            </section>
+            <section className={classes.tile}>
+                <h3 className={classes.tileTitle}>Extensions</h3>
+                    <ul className={classes.tileBody}>
+                        <li>
+                            <Link className={classes.link} to={resourceUrl('/extensions.html')}>
+                                Magento 1
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className={classes.link} to={resourceUrl('/extensions/magento2.html')}>
+                                Magento 2
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className={classes.link} to={resourceUrl('/extensions/magento2/free-m2.html')}>
+                                Free Stuff
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className={classes.link} to={resourceUrl('/terms')}>
+                                License Agreement
+                            </Link>
+                        </li>
+                    </ul>
+            </section>
+            <section className={classes.tile}>
+                <h3 className={classes.tileTitle}>Fooman</h3>
+                    <ul className={classes.tileBody}>
+                        <li>
+                            <Link className={classes.link} to={resourceUrl('/about-fooman')}>
+                                About Fooman
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className={classes.link} to={resourceUrl('/customer-profiles')}>
+                                Customer Case Studies
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className={classes.link} to={resourceUrl('/privacy')}>
+                                Privacy
+                            </Link>
+                        </li>
+                    </ul>
+            </section>
+            <section className={classes.tile}>
+                <h3 className={classes.tileTitle}>Support</h3>
+                    <ul className={classes.tileBody}>
+                        <li>
+                            <a style={classes.link} href='https://magento1-support.fooman.co.nz/'>
+                                Magento 1 Help Centre
+                            </a>
+                        </li>
+                        <li>
+                            <a style={classes.link} href='https://magento2-support.fooman.co.nz/'>
+                                Magento 2 Help Centre
+                            </a>
+                        </li>
+                        <li>
+                            <Link className={classes.link} to={resourceUrl('/contacts')}>
+                                Contact Support
+                            </Link>
+                        </li>
+                    </ul>
+            </section>
+        </footer>
+    );
+};
+
+Footer.propTypes = {
+    classes: shape({
+        copyright: string,
+        root: string,
+        tile: string,
+        tileBody: string,
+        tileTitle: string
+    })
+};
+
+export default Footer;
