e72f097eaf0e456dc279995db72907045df78ed6
smartsuhani <suhanishah228@gmail.com>
2020-07-07 20:07:40 +0530

    override category page with gallery

---
+++ src/overrides/venia-ui/components/Gallery/gallery.js
@@ -0,0 +1,49 @@
+import React, { useMemo } from 'react';
+import { string, shape, array } from 'prop-types';
+
+import { mergeClasses } from '@magento/venia-ui/lib/classify';
+import GalleryItem from './item';
+import defaultClasses from './gallery.css';
+
+const mapGalleryItem = item => {
+    const { small_image } = item;
+    return {
+        ...item,
+        small_image:
+            typeof small_image === 'object' ? small_image.url : small_image
+    };
+};
+
+const Gallery = props => {
+    const classes = mergeClasses(defaultClasses, props.classes);
+
+    const { items } = props;
+
+    const galleryItems = useMemo(
+        () =>
+            items.map((item, index) => {
+                if (item === null) {
+                    return <GalleryItem key={index} />;
+                }
+                return <GalleryItem key={index} item={mapGalleryItem(item)} />;
+            }),
+        [items]
+    );
+    return (
+        <div className={classes.root}>
+            <div className={classes.items}>{galleryItems}</div>
+        </div>
+    );
+};
+
+Gallery.propTypes = {
+    classes: shape({
+        filters: string,
+        items: string,
+        pagination: string,
+        root: string
+    }),
+    items: array.isRequired
+};
+
+export default Gallery;
---
+++ src/overrides/venia-ui/components/Gallery/item.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import { string, number, shape } from 'prop-types';
+import { Link, resourceUrl } from '@magento/venia-drivers';
+import { Price } from '@magento/peregrine';
+import { transparentPlaceholder } from '@magento/peregrine/lib/util/images';
+import { UNCONSTRAINED_SIZE_KEY } from '@magento/peregrine/lib/talons/Image/useImage';
+
+import { mergeClasses } from '@magento/venia-ui/lib/classify';
+import Image from '@magento/venia-ui/lib/components/Image';
+import StarRatingComponent from "react-star-rating-component";
+import defaultClasses from './item.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faStar} from "@fortawesome/free-solid-svg-icons";
+
+// The placeholder image is 4:5, so we should make sure to size our product
+// images appropriately.
+const IMAGE_WIDTH = 300;
+const IMAGE_HEIGHT = 375;
+
+// Gallery switches from two columns to three at 640px.
+const IMAGE_WIDTHS = new Map()
+    .set(640, IMAGE_WIDTH)
+    .set(UNCONSTRAINED_SIZE_KEY, 840);
+
+// TODO: get productUrlSuffix from graphql when it is ready
+const productUrlSuffix = '.html';
+
+const ItemPlaceholder = ({ classes }) => (
+    <div className={classes.root_pending}>
+        <div className={classes.images_pending}>
+            <Image
+                alt="Placeholder for gallery item image"
+                classes={{
+                    image: classes.image_pending,
+                    root: classes.imageContainer
+                }}
+                src={transparentPlaceholder}
+            />
+        </div>
+        <div className={classes.name_pending} />
+        <div className={classes.price_pending} />
+    </div>
+);
+
+const GalleryItem = props => {
+    const { item } = props;
+
+    const classes = mergeClasses(defaultClasses, props.classes);
+
+    if (!item) {
+        return <ItemPlaceholder classes={classes} />;
+    }
+
+    const { name, price, small_image, url_key } = item;
+    debugger
+    const productLink = resourceUrl(`/${url_key}${productUrlSuffix}`);
+    return (
+        <div className={classes.root}>
+            <Link to={productLink} className={classes.images}>
+                <Image
+                    alt={name}
+                    classes={{
+                        image: classes.image,
+                        root: classes.imageContainer
+                    }}
+                    height={IMAGE_HEIGHT}
+                    resource={small_image}
+                    widths={IMAGE_WIDTHS}
+                />
+            </Link>
+            <div className={classes.productDetail}>
+                <div className={classes.nameDiv}>
+                    <Link to={productLink} className={classes.name}>
+                        <span>{name}</span>
+                    </Link>
+                </div>
+                <div className={classes.starRating}>
+                    <div className={classes.ratReview}>
+                        <StarRatingComponent
+                            name="rating_summary"
+                            editing={false}
+                            renderStarIcon={() => <FontAwesomeIcon icon={faStar} size={'1x'}/>}
+                            starCount={5}
+                            value={5}
+                        />
+                    </div>
+                    <div className={classes.ratReview}>
+                        <text className={classes.reviews}>5 Reviews</text>
+                    </div>
+                </div>
+                <div className={classes.price}>
+                    <Price
+                        value={price.regularPrice.amount.value}
+                        currencyCode={price.regularPrice.amount.currency}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+GalleryItem.propTypes = {
+    classes: shape({
+        image: string,
+        imageContainer: string,
+        imagePlaceholder: string,
+        image_pending: string,
+        images: string,
+        images_pending: string,
+        name: string,
+        name_pending: string,
+        price: string,
+        price_pending: string,
+        root: string,
+        root_pending: string,
+        productDetail: string,
+        nameDiv: string,
+        pName: string,
+        starRating: string,
+        ratReview: string,
+        reviews: string
+    }),
+    item: shape({
+        id: number.isRequired,
+        name: string.isRequired,
+        small_image: string.isRequired,
+        url_key: string.isRequired,
+        price: shape({
+            regularPrice: shape({
+                amount: shape({
+                    value: number.isRequired,
+                    currency: string.isRequired
+                }).isRequired
+            }).isRequired
+        }).isRequired
+    })
+};
+
+export default GalleryItem;
