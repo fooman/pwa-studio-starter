3a591f7c69ba92f6132321370e6bdc61cef152a8
smartsuhani <suhanishah228@gmail.com>
2020-07-20 17:40:55 +0530

    add some css for static component for home

--- src/RootComponents/HomePage/homePage.js
+++ src/RootComponents/HomePage/homePage.js
@@ -7,19 +7,45 @@ import AgencyLogo from './AgencyLogoComponent/agencyLogo';
 import Team from './TeamComponent/team';
 import BackedByEveryThing from './BackedByEveryThingComponent/backedByEveryThing';
 import ReadyToSave from './ReadyToSaveComponent/readyToSave';
+import {mergeClasses} from "@magento/venia-ui/lib/classify";
+import defaultClasses from "./homePage.css";
+import {shape, string} from "prop-types";
 
 const HomePage = () => {
+    const classes = mergeClasses(defaultClasses);
     return (
         <div>
-            <HeroComponent/>
-            <TrustedBy/>
-            <Experts/>
-            <AgencyLogo/>
+            <div>
+              <HeroComponent/>
+            </div>
+            <div className={classes.trustedComponent}>
+             <TrustedBy/>
+            </div>
+            <div className={classes.commonComponent}>
+             <Experts/>
+            </div>
+            <div>
+             <AgencyLogo/>
+            </div>
+            <div className={classes.teamComponent}>
             <Team/>
+            </div>
+            <div className={classes.commonComponent}>
             <BackedByEveryThing/>
+            </div>
+            <div>
             <ReadyToSave/>
+            </div>
         </div>
     );
 }
+
+HomePage.propTypes = {
+    classes: shape({
+        trustedComponent: string,
+        commonComponent: string,
+        teamComponent: string
+    })
+};
 
 export default HomePage;
