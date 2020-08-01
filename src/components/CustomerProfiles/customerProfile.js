6f31b17727fbe263efeb9d6f4ed8f1fc94dff95b
smartsuhani <suhanishah228@gmail.com>
2020-08-01 19:09:03 +0530

    add root component testimonials

--- src/components/CustomerProfiles/customerProfile.js
+++ src/components/CustomerProfiles/customerProfile.js
@@ -1,9 +1,59 @@
 import React from "react";
+import { useQuery } from '@apollo/react-hooks';
+import testimonialQuery from './getTestimonialsForProfile.graphql';
+import { mergeClasses } from '@magento/venia-ui/lib/classify';
+import defaultClasses from './customerProfile.css';
+import ProfileContent from './profileContent';
+import { shape, string } from "prop-types";
 
 const CustomerProfile = () => {
-    return (<div>
-        <p>Customer Profile Page</p>
-        </div>);
+    const {data} = useQuery(testimonialQuery);
+    let items;
+    let itemLength;
+
+    items = data ? data.testimonials.items : [];
+    itemLength = items.length;
+
+    const classes = mergeClasses(defaultClasses);
+
+    const logoHandleClick = (url) => {
+        window.open(url, '_blank')
+    }
+
+    const mappedItems = () => {
+        return items && items.map((singleItem , index) => {
+            if (singleItem.content) {
+                return (
+                    <div key={index}>
+                        <ProfileContent
+                            singleItem = {singleItem}
+                            itemIndex = {index}
+                            itemLength = {itemLength}
+                            logoHandleClick = {logoHandleClick}
+                        />
+                    </div>
+                );
+            }
+        });
+    }
+
+    return (
+        <div className={classes.root}>
+            <div >
+                <h1 className={classes.heading}>{'Customer Profiles'}</h1>
+            </div>
+            <div>
+                {mappedItems()}
+            </div>
+        </div>
+    );
 }
+
+CustomerProfile.propTypes = {
+    classes: shape({
+        root: string,
+        heading: string
+    })
+};
 
 export default CustomerProfile;
