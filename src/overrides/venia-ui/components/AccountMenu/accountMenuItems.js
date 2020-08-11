0f40d88889b552863f486973894aa47c941cd0c7
smartsuhani <suhanishah228@gmail.com>
2020-08-11 20:22:37 +0530

    close navigation drawer

--- src/overrides/venia-ui/components/AccountMenu/accountMenuItems.js
+++ src/overrides/venia-ui/components/AccountMenu/accountMenuItems.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { func, shape, string } from 'prop-types';
 
-import { useDropdown } from '@magento/peregrine/lib/hooks/useDropdown';
+import { useAppContext } from '@magento/peregrine/lib/context/app';
 
 import { Link } from '@magento/venia-drivers';
 import { mergeClasses } from '@magento/venia-ui/lib/classify';
@@ -26,13 +26,11 @@ const AccountMenuItems = props => {
 
     const classes = mergeClasses(defaultClasses, props.classes);
 
-    const {
-        expanded: accountMenuIsOpen,
-        setExpanded: setAccountMenuIsOpen
-    } = useDropdown();
+    const [appState, { closeDrawer }] = useAppContext();
+    const { drawer } = appState;
 
     const handleAccountMenu = () => {
-        accountMenuIsOpen(false);
+        drawer === 'nav' ? closeDrawer() : null;
     }
 
     const menuItems = MENU_ITEMS.map(item => {
