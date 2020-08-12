0d5299e4059a8c8069e3124ad250286680bc9ac1
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-08-12 12:59:51 +1200

    Remove now unused

--- src/components/CustomRichContent/CustomRichContent.js
+++ src/components/CustomRichContent/CustomRichContent.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import parse , { domToReact } from 'html-react-parser';
-import Image from "@magento/venia-ui/lib/components/Image";
-import { mergeClasses } from '@magento/venia-ui/lib/classify';
+
 import RichContent from "@magento/venia-ui/lib/components/RichContent";
 import HighlightedCustomersComponent  from "./HighlightedCustomersComponent";
 import ProductFeatureComponent  from "./ProductFeatureComponent";
-import defaultClasses from "./CustomRichContent.css";
-import productFeatureClasses from "./ProductFeatureComponent.css";
-
-
-const classes = mergeClasses(defaultClasses,productFeatureClasses);
 
 function replaceHtmlWithReact({attribs, children, type, name}) {
     if (!attribs) {
@@ -22,16 +16,6 @@ function replaceHtmlWithReact({attribs, children, type, name}) {
             src={new URL(attribs.src).pathname}
             title={attribs.title}
         />;
-        /*return <Image
-            alt={name}
-            classes={{
-                image: classes.image,
-                root: classes.imageContainer
-            }}
-            height={IMAGE_HEIGHT}
-            resource={small_image}
-            widths={IMAGE_WIDTHS}
-        />*/
     }
     if (attribs.class === "media feature") {
         return <ProductFeatureComponent content={domToReact(children, {replace: replaceHtmlWithReact})}/>
