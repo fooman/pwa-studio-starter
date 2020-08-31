12e15b9302d0edcfc1fa28330a2d99cfb8ae4d9c
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-08-31 13:01:13 +1200

    New query location

---
+++ src/overrides/venia-ui/RootComponents/Product/product.gql.js
@@ -0,0 +1,138 @@
+import { gql } from '@apollo/client';
+
+export const GET_PRODUCT_DETAIL_QUERY = gql`
+    query getProductDetailForProductPage($urlKey: String!) {
+        products(filter: { url_key: { eq: $urlKey } }) {
+            items {
+                # Once graphql-ce/1027 is resolved, use a ProductDetails fragment
+                # here instead.
+                __typename
+                categories {
+                    id
+                    breadcrumbs {
+                        category_id
+                    }
+                }
+                description {
+                    html
+                }
+                short_description {
+                    html
+                }
+                id
+                media_gallery_entries {
+                    id
+                    label
+                    position
+                    disabled
+                    file
+                }
+                review_count
+                rating_summary
+                reviews (currentPage: 1, pageSize: 20) {
+                    items {
+                        created_at
+                        nickname
+                        summary
+                        text
+                        average_rating
+                    }
+                    page_info {
+                        total_pages
+                    }
+                }
+                meta_title
+                meta_keyword
+                meta_description
+                name
+                price {
+                    regularPrice {
+                        amount {
+                            currency
+                            value
+                        }
+                    }
+                }
+                sku
+                small_image {
+                    url
+                }
+                url_key
+                ...on CustomizableProductInterface {
+                    options {
+                        __typename
+                        option_id
+                        title
+                        sort_order
+                        required
+                        ...on CustomizableFieldOption {
+                            product_sku
+                            option_id
+                            title
+                            fieldValue: value {
+                                max_characters
+                                price
+                                price_type
+                                sku
+                            }
+                        }
+                        ...on CustomizableRadioOption {
+                            radioValue: value {
+                                option_type_id
+                                price
+                                title
+                            }
+                        }
+                    }
+                }
+                ... on ConfigurableProduct {
+                    configurable_options {
+                        attribute_code
+                        attribute_id
+                        id
+                        label
+                        values {
+                            default_label
+                            label
+                            store_label
+                            use_default_value
+                            value_index
+                            swatch_data {
+                                ... on ImageSwatchData {
+                                    thumbnail
+                                }
+                                value
+                            }
+                        }
+                    }
+                    variants {
+                        attributes {
+                            code
+                            value_index
+                        }
+                        product {
+                            id
+                            media_gallery_entries {
+                                id
+                                disabled
+                                file
+                                label
+                                position
+                            }
+                            sku
+                            stock_status
+                            price {
+                                regularPrice {
+                                    amount {
+                                        currency
+                                        value
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
