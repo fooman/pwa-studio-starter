4cee942614b4dfb9db21cfb5bde87df25b37a853
smartsuhani <suhanishah228@gmail.com>
2020-09-03 19:09:34 +0530

    add to cart product test cases

--- e2e/cypress/integration/specification/product/addProduct.spec.js
+++ e2e/cypress/integration/specification/product/addProduct.spec.js
@@ -1,7 +1,24 @@
 describe('ProductPage', () => {
 
-    it('should open home page', () => {
+    it('should open product detail page(simple product) and add to cart', () => {
         cy.visit('/magento-extension-speedster.html')
-        // cy.title().should('eq', 'Home Page - Fooman')
-    })
+        cy.get('div[class="productFullDetail-cartActions-elY productFullDetail-section-2jX productFullDetail-section-2jX"]').children().click();
+    });
+
+    it('Add to cart for product which has option to select with url field', () => {
+        cy.visit('/magento-extension-flexible-navigation-menu.html')
+        cy.fixture('../fixtures/productData').then(function(data) {
+            cy.get('input[name="options"]').type(data.product.urlInput1)
+        });
+        cy.get('div[class="productFullDetail-cartActions-elY productFullDetail-section-2jX productFullDetail-section-2jX"]').children().click();
+    });
+
+    it('Add to cart for product which has option to select with url field also with select installation option', () => {
+        cy.visit('/magento-extension-flexible-navigation-menu.html')
+        cy.fixture('../fixtures/productData').then(function(data) {
+            cy.get('input[name="options"]').type(data.product.urlInput2)
+        });
+        cy.get('input[value="35"]').check();
+        cy.get('div[class="productFullDetail-cartActions-elY productFullDetail-section-2jX productFullDetail-section-2jX"]').children().click();
+    });
 })
