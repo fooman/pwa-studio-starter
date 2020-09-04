4f7870d373886785754317ebf8b993225962378a
smartsuhani <suhanishah228@gmail.com>
2020-09-04 12:49:08 +0530

    change in fixtures use

--- e2e/cypress/integration/specification/product/addProduct.spec.js
+++ e2e/cypress/integration/specification/product/addProduct.spec.js
@@ -1,24 +1,27 @@
 describe('ProductPage', () => {
 
-    it('should open product detail page(simple product) and add to cart', () => {
-        cy.visit('/magento-extension-speedster.html')
-        cy.get('div[class="productFullDetail-cartActions-elY productFullDetail-section-2jX productFullDetail-section-2jX"]').children().click();
-    });
-
-    it('Add to cart for product which has option to select with url field', () => {
-        cy.visit('/magento-extension-flexible-navigation-menu.html')
-        cy.fixture('../fixtures/productData').then(function(data) {
-            cy.get('input[name="options"]').type(data.product.urlInput1)
+    it('should open product(free product) and add to cart', () => {
+        cy.fixture('../fixtures/productData').then(function (data){
+            cy.visit(data.freeProduct.url)
         });
         cy.get('div[class="productFullDetail-cartActions-elY productFullDetail-section-2jX productFullDetail-section-2jX"]').children().click();
     });
 
-    it('Add to cart for product which has option to select with url field also with select installation option', () => {
-        cy.visit('/magento-extension-flexible-navigation-menu.html')
+    it('should open free product which has option to select', () => {
         cy.fixture('../fixtures/productData').then(function(data) {
-            cy.get('input[name="options"]').type(data.product.urlInput2)
+            cy.visit(data.freeProductWithOption.url)
+            cy.get(`input[value=${data.freeProductWithOption.installationOptionValue}]`).check();
         });
-        cy.get('input[value="35"]').check();
+        // cy.get('input[name="options"]').type(data.product.urlInput1)
+        cy.get('div[class="productFullDetail-cartActions-elY productFullDetail-section-2jX productFullDetail-section-2jX"]').children().click();
+    });
+
+    it('should open paid product which has option to select with url field also with select installation option', () => {
+        cy.fixture('../fixtures/productData').then(function(data) {
+            cy.visit(data.paidProductWithOption.url)
+            cy.get(`input[name=${data.paidProductWithOption.urlInputName}]`).type(data.paidProductWithOption.urlInputValue)
+            cy.get(`input[value=${data.paidProductWithOption.installationOptionValue}]`).check();
+        });
         cy.get('div[class="productFullDetail-cartActions-elY productFullDetail-section-2jX productFullDetail-section-2jX"]').children().click();
     });
 })
