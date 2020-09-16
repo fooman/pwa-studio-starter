09d5203bbd9bc53ff962d1186c1c24a1f53b4aa0
smartsuhani <suhanishah228@gmail.com>
2020-09-16 15:12:51 +0530

    change in login test cases according to failur in cypress dashboard

--- e2e/cypress/integration/specification/loginSpec/login.spec.js
+++ e2e/cypress/integration/specification/loginSpec/login.spec.js
@@ -1,25 +1,51 @@
 describe('LoginTest', function() {
     // const login = new Login()
 
-    beforeEach(function() {
-        cy.log('It should open Login Page')
-        cy.visit('/')
-    })
+    const pressMyAccountBtn = () => {
+        cy.get('button[aria-label="Toggle My Account Menu"]').click();
+    }
 
-    it('should render error message if email is not put.', function() {
-        cy.get('button[aria-label="Toggle My Account Menu"]').click()
-    })
-
-    it('sign in with valid credentials.', function() {
-        cy.get('button[aria-label="Toggle My Account Menu"]').click()
-
+    const processForSignUp = () => {
+        pressMyAccountBtn();
         cy.fixture('../fixtures/loginData').then(function(data) {
             cy.get('input[name="email"]').last().type(data.user.email)
             cy.get('input[name="password"]').last().type(data.user.password)
         });
+
         cy.get('[data-testid="signIn-form"]').within(() => {
             cy.get('button[type="submit"]').first().click()
         })
+    }
+
+    beforeEach(function() {
+        cy.log('It should open Login Page')
+        cy.visit('/')
+    });
+
+    it('should render error message if email is not put.', function() {
+        cy.get("body").then($body => {
+            if ($body.find('svg[class*=indicator-indicator]').length > 0) {
+                cy.get('svg[class*=indicator-indicator]', { timeout: 40000 }).should('not.visible').then(() => {
+                    pressMyAccountBtn();
+                });
+            }
+            else pressMyAccountBtn();
+        });
+
+    })
+
+    it('sign in with valid credentials.', function() {
+        cy.get("body").then($body => {
+            if ($body.find('svg[class*=indicator-indicator]').length > 0) {
+                cy.get('svg[class*=indicator-indicator]', { timeout: 40000 }).should('not.visible').then(() => {
+                    processForSignUp();
+                });
+            }
+            else {
+                processForSignUp();
+            }
+        });
+
     })
 
 })
