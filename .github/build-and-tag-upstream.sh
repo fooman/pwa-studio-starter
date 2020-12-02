dfc56f163e13ecc6f5d245cce7b02cda2b98e9a2
Kristof Ringleff <kristof@fooman.co.nz>
2020-12-03 12:09:24 +1300

    clone from pwa-studio

--- .github/build-and-tag-upstream.sh
+++ .github/build-and-tag-upstream.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-pwd
-ls -al
-echo "$1"
+TAG=$1
+echo "$TAG"
 SHA_LOOKUP=${1//[.]/_}
 echo "$SHA_LOOKUP"
 source $GITHUB_WORKSPACE/.github/tags-to-build
@@ -9,12 +8,27 @@ if git rev-parse $1 >/dev/null 2>&1
 then
     echo "Found tag"
 else
+    echo "Retrieve PWA-Studio"
+    git clone https://github.com/magento/pwa-studio.git $GITHUB_WORKSPACE/../pwa-studio-$TAG
+
 	echo "COMMIT to USE" ${!SHA_LOOKUP}
+    cd $GITHUB_WORKSPACE/../pwa-studio-$TAG && git checkout ${!SHA_LOOKUP}
+
+    echo "NPM Install"
+    cd $GITHUB_WORKSPACE/../pwa-studio-$TAG/packages/pwa-buildpack && npm install
+    
+    echo "create packages"
+    cd $GITHUB_WORKSPACE/../pwa-studio && DEBUG_PROJECT_CREATION=true ./packages/pwa-buildpack/bin/buildpack create-project ../$TAG-to-delete --template "venia-concept" --name "pwa-studio-canary" --author "Test Author<user@example.com>" --backend-url "https://master-7rqtwti-mfwmkrjfqvbjk.us-4.magentosite.cloud/" --braintree-token "sandbox_8yrzsvtm_s2bg8fs563crhqzk" --npm-client "yarn" --install 0
+    echo "Debug output"
+    ls -al $GITHUB_WORKSPACE/../
+    cat $GITHUB_WORKSPACE/../tmp-to-delete/package.json
+
+    echo "Update code"
+
     cd $GITHUB_WORKSPACE
-    git checkout $COMMIT_SHA
-    ls -al
-    cd $GITHUB_WORKSPACE/packages/pwa-buildpack && npm install
-    DEBUG_PROJECT_CREATION=true $GITHUB_WORKSPACE/packages/pwa-buildpack/bin/buildpack create-project $GITHUB_WORKSPACE/../$1-to-delete --template "venia-concept" --name "pwa-studio" --author "Test Author<user@example.com>" --backend-url "https://master-7rqtwti-mfwmkrjfqvbjk.us-4.magentosite.cloud/" --braintree-token "sandbox_8yrzsvtm_s2bg8fs563crhqzk" --npm-client "yarn" --install 0
-    cp $GITHUB_WORKSPACE/../$TAG-to-delete/package.json > $GITHUB_WORKSPACE/output/packages/pwa-create/package-$1.json
-    cat $GITHUB_WORKSPACE/output/packages/pwa-create/package-$1.json
+    find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf
+    cp -r $GITHUB_WORKSPACE/../$TAG-to-delete/. .
+    rm $GITHUB_WORKSPACE/yarn.lock
+
+	git status
 fi
