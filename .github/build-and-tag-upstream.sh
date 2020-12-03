e8806e22f7831fd87974622f6967f0c7fd836504
Kristof Ringleff <kristof@fooman.co.nz>
2020-12-03 13:40:14 +1300

    narrow down issue

--- .github/build-and-tag-upstream.sh
+++ .github/build-and-tag-upstream.sh
@@ -22,7 +22,10 @@ else
     
     echo "remove local resolutions"
 
+    cat $GITHUB_WORKSPACE/../$TAG-to-delete/package.json
+    cat $GITHUB_WORKSPACE/../$TAG-to-delete/package.json | jq 'del(.resolutions)'
 	cat $GITHUB_WORKSPACE/../$TAG-to-delete/package.json | jq 'del(.resolutions)' > $GITHUB_WORKSPACE/../$TAG-to-delete/package.json
+	cat $GITHUB_WORKSPACE/../$TAG-to-delete/package.json
     export MY_PATH=$(readlink -f $GITHUB_WORKSPACE/../pwa-studio-$TAG/packages) && sed -i -e "s#file://$MY_PATH/[^0-9]*##g" $GITHUB_WORKSPACE/../$TAG-to-delete/package.json && sed -i -e "s#\.tgz##g" $GITHUB_WORKSPACE/../$TAG-to-delete/package.json
     echo "Update code"
 
