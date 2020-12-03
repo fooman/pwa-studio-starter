96abf27cfb4c70cf25f8486600154b54562110d1
Kristof Ringleff <kristof@fooman.co.nz>
2020-12-03 15:04:44 +1300

    remove tmp file, tag commit

--- .github/build-and-tag-upstream.sh
+++ .github/build-and-tag-upstream.sh
@@ -24,11 +24,12 @@ else
 
     mv $GITHUB_WORKSPACE/../$TAG-to-delete/package.json $GITHUB_WORKSPACE/../$TAG-to-delete/package-tmp.json
 	cat $GITHUB_WORKSPACE/../$TAG-to-delete/package-tmp.json | jq 'del(.resolutions)' > $GITHUB_WORKSPACE/../$TAG-to-delete/package.json
-	cat $GITHUB_WORKSPACE/../$TAG-to-delete/package.json
-    export MY_PATH=$(readlink -f $GITHUB_WORKSPACE/../pwa-studio-$TAG/packages) && sed -i -e "s#file://$MY_PATH/[^0-9]*##g" $GITHUB_WORKSPACE/../$TAG-to-delete/package.json && sed -i -e "s#\.tgz##g" $GITHUB_WORKSPACE/../$TAG-to-delete/package.json
+	rm $GITHUB_WORKSPACE/../$TAG-to-delete/package-tmp.json
+    export MY_PATH=$(readlink -f $GITHUB_WORKSPACE/../pwa-studio-$TAG/packages) && sed -i -e "s#file://$MY_PATH/[^0-9]*#~#g" $GITHUB_WORKSPACE/../$TAG-to-delete/package.json && sed -i -e "s#\.tgz##g" $GITHUB_WORKSPACE/../$TAG-to-delete/package.json
     echo "Update code"
 
     cd $GITHUB_WORKSPACE
+    git pull
     find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf
     cp -r $GITHUB_WORKSPACE/../$TAG-to-delete/. .
     rm $GITHUB_WORKSPACE/yarn.lock
